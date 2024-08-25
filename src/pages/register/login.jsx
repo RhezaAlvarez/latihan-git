935e42ed4596a48f08a549bfbd3c1cc33e138e34
RhezaAlvarez <rhezaalvarez@gmail.com>
2024-08-25 13:42:08 +0700

    Register

---
+++ src/pages/register/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+    return(
+        <div className="w-[100vh] h-[100vh] bg-white text-black">
+            Register
+        </div>
+    )
+}
+
+export default Register
