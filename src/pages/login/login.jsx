c5833ef30cfb1b6bbb2ff1dab797287fa007329c
vivaldifikry <vivaldifikry2@gmail.com>
2024-08-25 13:59:48 +0700

    text a login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="w-[100vh] h-[100vh] bg-white text-black">LOGIN pon</div>
+    <div className="w-[100vh] h-[100vh] bg-white text-black">
+      LOGIN pon
+      <span>text A</span>
+    </div>
   );
 };
 
